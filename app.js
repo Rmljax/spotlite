e6aa1b0644a188734c361888794d60e62d09e9bc
Rmljax <ryan.lockenbach@gmail.com>
2026-02-23 15:36:38 -0500

    finished project

---
+++ app.js
@@ -0,0 +1,95 @@
+apiKey = "372e15b51b6346d3ac68a49e473ac3bd";
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNzJlMTViNTFiNjM0NmQzYWM2OGE0OWU0NzNhYzNiZCIsIm5iZiI6MTc2ODcyMzc0Ny45Njk5OTk4LCJzdWIiOiI2OTZjOTUyMzZmNGViMzE2ZDFhYWE4NmEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.u3ASu7qqFHvYDBix8T0oqqpC1Fwet1Om4zOJgOI1Byg",
+  },
+};
+async function fetchMovies() {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/discover/movie?`,
+    options,
+  );
+  const data = await response.json();
+  const movies = data.results;
+  return movies;
+}
+
+async function fetchSearch(keyWord) {
+  const response = await fetch(
+    `https://api.themoviedb.org/3/search/movie?query=${keyWord}`,
+    options,
+  );
+  const data = await response.json();
+  const movies = data.results;
+  return movies;
+}
+
+async function renderMovies(searchValue = null) {
+  const moviesList = document.querySelector("#movies-list");
+  let movies = await fetchMovies();
+  console.log(movies);
+
+  if (searchValue !== null) {
+    movies = await fetchSearch(searchValue);
+    console.log(movies);
+  }
+  moviesList.innerHTML = movies
+    .slice(0, 6)
+    .map(
+      (movie) => `<div class="movie">
+                            <figure class="movie__image__wrapper">
+                                <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="" class="movie__image">
+                                <h3 class="movie__info__title">${movie.title}</h3>
+                                <div class="movie__info__list">
+                                    <div class="movie__info movie__info1">
+                                        <i class="fa-solid fa-clock"></i>
+                                        <p class="movie__info__text">${movie.release_date}</p>
+                                    </div>
+                                    <div class="movie__info movie__info2">
+                                        <i class="fa-solid fa-star"></i>
+                                        <p class="movie__info__text">${movie.vote_average.toFixed(1)}</p>
+                                    </div>
+                                    <div class="movie__info movie__info3">
+                                        <i class="fa-solid fa-earth-americas"></i>
+                                        <p class="movie__info__text">${movie.original_language.toUpperCase()}</p>
+                                    </div>
+                                </div>
+                            </figure>
+                            <h4 class="movie__title">${movie.title}</h4>
+                        </div>`,
+    )
+    .join("");
+}
+
+function searchBarEvent(event) {
+  if (event.key === "Enter") {
+    const searchValue = document.querySelector("#search-input").value;
+    document.querySelector("#search-input").value = "";
+    document.querySelector("#search-text").innerHTML = `"${searchValue}"`;
+    renderMovies(searchValue);
+  }
+}
+function searchNavEvent(event) {
+  if (event.key === "Enter") {
+    const searchValue = document.querySelector("#nav-input").value;
+    document.querySelector("#nav-input").value = "";
+    document.querySelector("#search-text").innerHTML = `"${searchValue}"`;
+    renderMovies(searchValue);
+  }
+}
+
+function searchBarClick() {
+  const searchValue = document.querySelector("#search-input").value;
+  document.querySelector("#search-input").value = "";
+  document.querySelector("#search-text").innerHTML = `"${searchValue}"`;
+  renderMovies(searchValue);
+}
+
+function searchActive() {
+  document.querySelector(".nav__input").focus();
+}
+
+renderMovies();
